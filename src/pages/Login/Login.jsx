6037ebdf03449a99109a7662acf993e2b2b80215
IshaqQuresheeAkib <akibqureshee@gmail.com>
2023-10-19 02:05:36 +0600

    created responsive login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,55 +4,44 @@ import { AiFillEye } from 'react-icons/ai';
 
 const Login = () => {
     return (
-        <section className="flex flex-wrap justify-center lg:h-screen lg:items-stretch my-20 md:order-1 order-2">
-            <div className="relative h-64  sm:h-96 lg:h-full ">
-                    <img
-                    alt="Welcome"
-                    src="https://i.ibb.co/smbnvN8/olav-tvedt-o-Va-YMg-BMbs-unsplash-1.jpg"
-                    className="h-3/4 object-contain rounded-xl"
-                    />
-            </div>
-            <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24 ">
-
+        <section className="flex flex-wrap justify-center lg:h-screen lg:items-end my-20 md:order-1 order-2">
+            <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24 h-3/4">
                 <div className="mx-auto max-w-lg text-center">
                     <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
-
                     <p className="mt-4 text-gray-500">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
                         eaque error neque ipsa culpa autem, at itaque nostrum!
                     </p>
                 </div>
-                <form action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+                <form className="mx-auto mb-0 mt-8 max-w-md space-y-4">
                 <div>
-                        <label className="sr-only">Email</label>
-                        <div className="relative">
+                    <label className="sr-only">Email</label>
+                    <div className="relative">
                         <input
                             type="email"
+                            name='email'
                             className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                             placeholder="Enter email"
                         />
-
-                        <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                        <span className="absolute inset-y-0 text-xl end-0 grid place-content-center px-4">
                             <MdAlternateEmail></MdAlternateEmail>
                         </span>
-                        </div>
                     </div>
-
-                    <div>
-                        <label className="sr-only">Password</label>
-
-                        <div className="relative">
+                </div>
+                <div>
+                    <label className="sr-only">Password</label>
+                    <div className="relative">
                         <input
                             type="password"
+                            name="password"
                             className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                             placeholder="Enter password"
                         />
-
-                        <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                        <span className="absolute text-xl inset-y-0 end-0 grid place-content-center px-4">
                             <AiFillEye></AiFillEye>
                         </span>
-                        </div>
                     </div>
+                </div>
 
                     <div className="flex items-center justify-between">
                         <p className="text-sm text-gray-500">
