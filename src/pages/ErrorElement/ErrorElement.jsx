d0e8dc0b06971e06eaa7dee9774ae12317a84eac
IshaqQuresheeAkib <akibqureshee@gmail.com>
2023-10-20 05:47:15 +0600

    created responsive error page

--- src/pages/ErrorElement/ErrorElement.jsx
+++ src/pages/ErrorElement/ErrorElement.jsx
@@ -1,7 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+
 const ErrorElement = () => {
+
+    const navigate = useNavigate();
+
     return (
         <div>
-            <h1>Error 404</h1>
+           <section className="errorPage">
+        <div className="errorPage__text ">
+            <div>
+            <h3>404</h3>
+            </div>
+            <button onClick={()=>navigate('/')} className="errorPage__button">Back to home</button>
+        </div>
+        <span className="errorPage__templeweed-container"><img src="https://i.ibb.co/mTg87G2/tembleweed.png" className="errorPage__tembleweed"/></span>
+        <div className="errorPage__terrain"></div>
+    </section>
         </div>
     );
 };
